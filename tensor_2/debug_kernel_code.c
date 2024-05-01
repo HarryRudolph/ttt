d2e2f19616ca5bc2da8846353153ca4d03aa11a2
harryrudolph <harryrudolph@me.com>
2024-05-01 20:47:42 +0100

    non-meaningful commit message

---
+++ tensor_2/debug_kernel_code.c
@@ -0,0 +1,11 @@
+#include <metal_stdlib>
+using namespace metal;
+kernel void r_2(device int* data0, const device int* data1, const device int* data2, uint3 gid [[threadgroup_position_in_grid]], uint3 lid [[thread_position_in_threadgroup]]) {
+  int acc0 = 0;
+  for (int ridx0 = 0; ridx0 < 2; ridx0++) {
+    int val0 = *(data1+ridx0);
+    int val1 = *(data2+ridx0);
+    acc0 = ((val0*val1)+acc0);
+  }
+  *(data0+0) = acc0;
+}
---
+++ tensor_2/notes.md
@@ -0,0 +1,7 @@
+# Understanding Tinygrad
+
+debug_kernel_code.c is pretty cool.
+
+To read:
+https://developer.apple.com/documentation/metal/compute_passes/creating_threads_and_threadgroups
+codegen utility
